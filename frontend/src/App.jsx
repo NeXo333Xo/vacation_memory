408317e068c1cb7dc72468be27eb5bfe5aeba051
NeXo333Xo <marlon.schrader30512464@gmail.com>
2025-11-13 18:51:08 +0100

    added daisyUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,6 +29,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
     </>
   )
 }
