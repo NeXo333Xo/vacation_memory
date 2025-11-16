78cf50c5445f6cad137ced619eced2adb623ecc0
NeXo333Xo <marlon.schrader30512464@gmail.com>
2025-11-16 20:47:00 +0100

    main changed for allwoing api calls (from the frontend)

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from backend.api.first import router as first_router
+from backend.database import create_db_and_tables
 
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+from contextlib import asynccontextmanager
 
 
-@app.get("/items")
-def read_items():
-    return [{"item_id": "Foo"}, {"item_id": "Bar"}]
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(first_router, prefix="/api")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
